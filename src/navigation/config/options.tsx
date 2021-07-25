55f202f3999404d51935c9ba306da13817b1ff7b
Nikola Prijovic <nikolaprijovic@Nikolas-MacBook-Air.local>
2021-07-25 14:47:56 +0200

    Fixed no displayname issue on Options.tsx

--- src/navigation/config/options.tsx
+++ src/navigation/config/options.tsx
@@ -5,15 +5,17 @@ import * as RootNavigation from '../RootNavigation';
 
 const { height, width } = Dimensions.get('screen');
 
+const headerLeft = (): JSX.Element => (
+    <TouchableOpacity onPress={() => RootNavigation.goBack()}>
+        <MaterialIcon name="arrow-back" size={width >= 600 ? 32 : 26} />
+    </TouchableOpacity>
+);
+
 export const options = {
     headerStyle: {
         height: height / 10,
         elevation: 0,
         shadowOpacity: 0,
     },
-    headerLeft: () => (
-        <TouchableOpacity onPress={() => RootNavigation.goBack()}>
-            <MaterialIcon name="arrow-back" size={width >= 600 ? 32 : 26} />
-        </TouchableOpacity>
-    ),
+    headerLeft,
 };
