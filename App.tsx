7cb2ff171b4861f3e5c5c5b499ace4965c5c0f44
Nikola Prijovic <nikolaprijovic@Nikolas-MacBook-Air.local>
2021-08-08 20:32:40 +0200

    Added rnconfig, multi release on android

--- App.tsx
+++ App.tsx
@@ -7,46 +7,45 @@
  *
  * @format
  */
-
-import {NavigationContainer} from '@react-navigation/native';
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, View} from 'react-native';
-import NoInternet from './src/components/screens/NoInternet/NoInternet';
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, View } from 'react-native';
+import NoInternet from './src/components/screens/NoInternet';
 import Router from './src/navigation/Router';
 import NetInfo from '@react-native-community/netinfo';
-import {navigationRef} from './src/navigation/RootNavigation';
+import { navigationRef } from './src/navigation/RootNavigation';
 import ContextProvider from './src/modules/context/Context';
 import FlashMessage from 'react-native-flash-message';
 import Loader from './src/components/loader/Loader';
 
 const App = () => {
-  const [isConnected, setIsConnected] = useState(true);
+    const [isConnected, setIsConnected] = useState(true);
 
-  useEffect(() => {
-    NetInfo.addEventListener(state => setIsConnected(state.isConnected));
-  }, [isConnected]);
+    useEffect(() => {
+        NetInfo.addEventListener((state) => setIsConnected(state.isConnected));
+    }, [isConnected]);
 
-  return (
-    <NavigationContainer ref={navigationRef}>
-      <View style={styles.container}>
-        {isConnected ? (
-          <ContextProvider>
-            <Router />
-          </ContextProvider>
-        ) : (
-          <NoInternet />
-        )}
-        <Loader />
-        <FlashMessage position="top" />
-      </View>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer ref={navigationRef}>
+            <View style={styles.container}>
+                {isConnected ? (
+                    <ContextProvider>
+                        <Router />
+                    </ContextProvider>
+                ) : (
+                    <NoInternet />
+                )}
+                <Loader />
+                <FlashMessage position="top" />
+            </View>
+        </NavigationContainer>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 });
 
 export default App;
